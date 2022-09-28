6cd7249ff5c775606c792f224d9ce706cb09faff
Alex.Sotkovsky <alex.sotkovsky@cardinalhealth.com>
2022-09-28 17:04:22 -0400

    Starting project

--- src/main/kotlin/PayCalculator.kt
+++ src/main/kotlin/PayCalculator.kt
@@ -1,12 +1,17 @@
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.util.*
 
 private const val minStartHour = 17
 private const val maxEndHour = 28
 
 
-fun verifyValidStartTime(startTime: LocalDateTime): Boolean{
+fun verifyValidStartTime(startTime: LocalTime): Boolean{
     return startTime.hour > minStartHour
+}
+
+fun verifyValidEndTime(endTime: LocalTime): Boolean{
+    return endTime.hour < maxEndHour
 }
 
 //fun fetchHour(startTime: LocalDateTime): Int{
--- src/test/kotlin/PayCalculatorTest.kt
+++ src/test/kotlin/PayCalculatorTest.kt
@@ -1,14 +1,30 @@
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 import java.util.*
+import kotlin.test.assertEquals
 
 class PayCalculatorTest {
 
     @Test
-    fun startTimeNotBefore5PM(){
-        val startTime = LocalDateTime.now()
-        verifyValidStartTime(startTime)
+    fun verifyStartTimeBefore5PMReturnsFalse(){
+        val startTime = LocalTime.parse("10:15:45")
+        val actual = verifyValidStartTime(startTime)
+        assertEquals(false, actual)
+    }
+
+    @Test
+    fun verifyStartTimeAfter5PMReturnsTrue(){
+        val startTime = LocalTime.parse("20:15:00")
+        val actual = verifyValidStartTime(startTime)
+        assertEquals(true, actual)
+    }
+    @Test
+    fun verifyEndTimeBefore4AMReturnsTrue(){
+        val endTime = LocalTime.parse("01:00:00")
+        val actual = verifyValidEndTime(endTime)
+        assertEquals(true, actual)
     }
 
 }
